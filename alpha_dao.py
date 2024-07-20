e891ad9b27a55d1fb39af8ef373758e1ea276934
SK <sookiat.chang@gmail.com>
2024-07-20 19:27:49 +0800

    Added aggregating codes

--- alpha_dao.py
+++ alpha_dao.py
@@ -10,6 +10,11 @@ class AlphaDAO:
         self.base_url = "https://www.alphavantage.co/query?"
         self.file_path = "data"
 
+    def refresh_financials(self, symbol:str) -> None:
+        self.get_balance_sheet(symbol=symbol)
+        self.get_cashflow_statement(symbol=symbol)
+        self.get_income_statement(symbol=symbol)
+        
     def get_balance_sheet(self, symbol: str) -> None:
         params = {
             "function": "BALANCE_SHEET",
@@ -40,8 +45,21 @@ class AlphaDAO:
         self._dump_to_file(data=r.json(), name=symbol+"-cashflow_statement")
         # return r.json()
     
-    def _dump_to_file(self, data: dict, name:str):
+    def _dump_to_file(self, data: dict, name:str) -> None:
         file_path = os.path.join(self.file_path, name+".json")
         with open(file_path, 'w') as f: 
             json.dump(data, f)
+
+    def load_financials(self, symbol) -> dict:
+        fin_type = ["balance_sheet", "income_statement", "cashflow_statement"]    
+        fin_data = {}
+
+        for statement in fin_type:
+            file_path = os.path.join("data",symbol+"-" + statement + ".json")
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            fin_data[statement] = data
+
+        return fin_data
+
 
